0febdb8734f3cd5765b6381246b27b74934ec1e0
CarmenIJsebaart <cijsebaart@gmail.com>
2018-01-08 18:23:22 +0100

    Simplified again

--- main.cpp
+++ main.cpp
@@ -33,35 +33,40 @@ int sum_proper_devisors(std::vector<int> proper_divisors, int is_perfect)
   return sum;
 }
 
+int is_perfect_number(const int value)
+{
+  // Is this a perfect number?
+  // -1: unknown
+  //  0: false
+  //  1: true
+  int is_perfect{-1};
+
+  // Negative values are not perfect
+  if (value < 0) is_perfect = 0;
+
+  // Zero is not perfect
+  if (is_perfect == -1 && value == 0) is_perfect = 0;
+
+  //Collect the proper divisors
+  std::vector<int> proper_divisors = collect_proper_devisors(value, is_perfect);
+
+  //sum the proper divisors, if not known if number is perfect
+  int sum = sum_proper_devisors(proper_divisors, is_perfect);
+  if (is_perfect == -1 && sum == value) is_perfect = 1;
+  if (is_perfect == -1) is_perfect = 0;
+
+  //show
+  assert(is_perfect != -1); //Must be known now
+  return is_perfect;
+}
+
 int do_main(const std::vector<std::string> &args)
 {
   if (args.size() != 2) return 1;
   try
   {
     const int value{std::stoi(args[1])};
-
-    // Is this a perfect number?
-    // -1: unknown
-    //  0: false
-    //  1: true
-    int is_perfect{-1};
-
-    // Negative values are not perfect
-    if (value < 0) is_perfect = 0;
-
-    // Zero is not perfect
-    if (is_perfect == -1 && value == 0) is_perfect = 0;
-
-    //Collect the proper divisors
-    std::vector<int> proper_divisors = collect_proper_devisors(value, is_perfect);
-
-    //sum the proper divisors, if not known if number is perfect
-    int sum = sum_proper_devisors(proper_divisors, is_perfect);
-    if (is_perfect == -1 && sum == value) is_perfect = 1;
-    if (is_perfect == -1) is_perfect = 0;
-
-    //show
-    assert(is_perfect != -1); //Must be known now
+    int is_perfect = is_perfect_number(value);
     if (is_perfect == 1)
     {
       std::cout << "true\n";
